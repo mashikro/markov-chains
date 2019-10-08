1b254f264ed5b431b584a1c18ad482730d936722
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-08 13:03:49 -0700

    WIP: make_text

--- markov.py
+++ markov.py
@@ -1,6 +1,6 @@
 """Generate Markov text from text files."""
 
-from random import choice
+import random 
 
 
 def open_and_read_file(file_path):
@@ -50,28 +50,18 @@ def make_chains(text_string):
 
     word_list = text_string.split()
 
-    # for i, word in enumerate(word_list):
-    #     # slice from i to i + 2 ==> list of 2 words
-    #     # make that into a tuple
-    #     key = tuple(word_list[i:i+2])
-    #     # check dict if it has that tuple as a key
-    #         # if no, create list as value, first item = 
-    #     chains[key] = chains.get(key, []) + [word_list[i+2]]
-
     for i in range(len(word_list)-2):
-        # slice from i to i + 2 ==> list of 2 words
-        # make that into a tuple
+        # slice from i to i + 2 ==> list of 2 words. make that into a tuple
         key = tuple(word_list[i:i+2])
-        # check dict if it has that tuple as a key
-            # if no, create list as value, first item = 
+        # check if item in dict if key not assign an (empty list + [word_list[i+2]]) to a key
         chains[key] = chains.get(key, []) + [word_list[i+2]]
 
-    for key, value in chains.items():
-        print(f'{key}: {value}')
+    # for key, value in chains.items():
+    #     print(f'{key}: {value}')
 
     return chains
 
-make_chains(open_and_read_file('green-eggs.txt'))
+# make_chains(open_and_read_file('green-eggs.txt'))
 
 def make_text(chains):
     """Return text from chains."""
@@ -79,19 +69,31 @@ def make_text(chains):
     words = []
 
     # your code goes here
+    initial_key = random.choice(list(chains.keys())) #random.choice iterates over a list
+    words.extend(list(initial_key))
+
+    #while loop - until no more key left in dict
+    while not (words[-1] == 'am?'):
+        #new_key = second word in first key + random word from its value
+        random_word = random.choice(chains[new_key])
+        words.append(random_word)  #add random to words list 
+        #look_up the new_key in dict and if it exists we repeat above line
+        new_key = tuple(words[-2:])
+
 
     return " ".join(words)
 
+make_text(make_chains(open_and_read_file('green-eggs.txt')))
 
-input_path = "green-eggs.txt"
+# input_path = "green-eggs.txt"
 
-# Open the file and turn it into one long string
-input_text = open_and_read_file(input_path)
+# # Open the file and turn it into one long string
+# input_text = open_and_read_file(input_path)
 
-# Get a Markov chain
-chains = make_chains(input_text)
+# # Get a Markov chain
+# chains = make_chains(input_text)
 
-# Produce random text
-random_text = make_text(chains)
+# # Produce random text
+# random_text = make_text(chains)
 
-print(random_text)
+# print(random_text)
